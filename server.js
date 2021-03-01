bed99a49c802614839edaff3077354303ceb4fcc
Sam Sakaguchi <samsakaguchi@seattleacademy.org>
2021-03-01 12:34:15 -0800

    Uploaded more links and add pictures

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+const port = 1528; //set port
+if(port == 0){
+	console.log('set port in server.js')
+	process.exit(1);
+}
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+ 
+server.use(middlewares)
+server.use(router)
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`)
+})
